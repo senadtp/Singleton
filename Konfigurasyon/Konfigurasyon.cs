b43c9ce7bf3b4ab5d6b010b27fa7552fecb35b82
senadtp28@gmail.com <senadtp28@gmail.com>
2021-08-19 18:57:31 +0300

    first commit

---
+++ Konfigurasyon/Konfigurasyon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konfigurasyon
+{
+    public class Konfigurasyon
+    {
+        public  int MaksKullaniciSayisi { get; set; }
+        public  string LogDosyaAdi { get; set; }
+        private Konfigurasyon() {
+            this.MaksKullaniciSayisi = 10;
+            this.LogDosyaAdi = "D:\\USER.LOG";
+        }
+        private static Konfigurasyon instance;
+        public static Konfigurasyon getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Konfigurasyon();
+                Console.WriteLine(instance.LogDosyaAdi);
+                Console.WriteLine(instance.MaksKullaniciSayisi);
+                Console.WriteLine("Yeni konf yaratildi :)");
+            }
+            else
+            {
+                Console.WriteLine("Yeni konf yaratilamadi.:(");
+            }
+            return instance;
+        }
+    }
+}
---
+++ Konfigurasyon/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Konfigurasyon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Konfigurasyon konf1 = Konfigurasyon.getInstance();
+            Konfigurasyon konf2 = Konfigurasyon.getInstance();
+            Console.ReadKey();
+
+           
+        }
+    }
+}
